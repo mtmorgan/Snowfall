2fdcceb75c790662b763f1b99775d647a3086b60
Martin Morgan <martin.morgan@roswellpark.org>
2017-01-31 13:38:47 -0500

    Initial development of 'hi()'
    - Completed DESCRIPTION file
    - implemented hi()
    - documented hi() using roxygen2

---
+++ R/hi.R
@@ -0,0 +1,31 @@
+#' Start a conversation
+#' 
+#' This function will produce a friendly (or other) greeting, useful in
+#' starting conversations.
+#' 
+#' @param who character(1) Name of person to be greeted.
+#' 
+#' @param how character(1) (optional). How is the person to be greeted. Options
+#'     include "shout", "whisper", or "asis".
+#'     
+#' @return A character(1) vector containing the appropriate greeting.
+#' 
+#' @examples
+#' hi("Martin")
+#' \dontrun{
+#'     hi("Martin", "whisper")
+#' }
+#' 
+#' @export
+hi <- function(who, how=c("shout", "whisper", "asis")) {
+    stopifnot(
+        is.character(who), length(who) == 1,
+        !anyNA(who), nzchar(who)
+    )      
+    how <- match.arg(how)
+
+    fun <- switch(how, 
+        shout=toupper, whisper=tolower, asis=identity,
+    )
+    paste("hi", fun(who), "how are you?")
+}
