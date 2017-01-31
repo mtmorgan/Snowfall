e1bce9dd74460a4f17a584ba580691c555b2368f
Martin Morgan <martin.morgan@roswellpark.org>
2017-01-31 14:44:54 -0500

    test 'how' argument to 'hi()'

--- tests/testthat/test_hi.R
+++ tests/testthat/test_hi.R
@@ -2,6 +2,18 @@ test_that("hi() only accepts appropriate 'who'", {
     expect_equal(hi("Martin"), "hi MARTIN how are you?")
 })
 
+test_that("hi() only accepts appropriate 'how'", {
+    expect_equal(
+        hi("Martin", how="whisper"),
+        "hi martin how are you?"
+    )
+    expect_equal(
+        hi("MarTin", how="asis"),
+        "hi MarTin how are you?"
+    )
+    expect_error(hi("Martin", how="loud"))
+})
+
 test_that("is_scalar_character works", {
     expect_true(is_scalar_character("foo"))
     expect_false(is_scalar_character(123))
